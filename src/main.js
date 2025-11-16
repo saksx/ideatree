6e22b41903064fcfb47dc7c03e3f12f20a9227c7
saksx <sakshampandey2005@gmail.com>
2025-11-16 16:49:26 -0600

    basic stuff

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene()
+
+const cubeGeometry = new THREE.BoxGeometry(1,1,1)
+const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
+
+const cubeMesh = new THREE.Mesh(
+    cubeGeometry,
+    cubeMaterial
+
+)
+
+scene.add(cubeMesh)
+
+// initialize camera
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth / window.innerHeight, 
+    0.1,
+    30)
+
+camera.position.z = 5
+scene.add(camera)
+
+//initialize the render
